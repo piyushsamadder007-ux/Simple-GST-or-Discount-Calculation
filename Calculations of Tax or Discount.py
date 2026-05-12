bc41a505ea19b5c7e1fbc318e0f03202dbae5c6d
Piyush Samadder <piyushsamadder007@gmail.com>
2026-05-12 22:11:44 +0530

    Update Calculations of Tax or Discount.py

--- Calculations of Tax or Discount.py
+++ Calculations of Tax or Discount.py
@@ -8,12 +8,12 @@ while a=="":
         n1=float(input("\nEnter the price of the item: "))
         n2=float(input("Quantity: "))
         n3=float(input("GST percentage: "))
-        print("The gross amount is: ", n1+(n1*n2)*n3/100,"\nWhere, your GST amount is: ", (n1*n2)*n3/100)
+        print("\nThe gross amount is: ", n1+(n1*n2)*n3/100,"\nWhere, your GST amount is: ", (n1*n2)*n3/100)
     elif b=="D" or b=="d":
         print("\nWelcome to Discount calculator\nHere, you can calculate the Discount")
         n1=float(input("\nEnter the price of the item: "))
         n2=float(input("Quantity: "))
         n3=float(input("Discount percentage: "))
-        print("The gross amount is: ", n1-(n1*n2)*n3/100,"\nWhere, your discount amount is: ", (n1*n2)*n3/100)
+        print("\nThe gross amount is: ", n1-(n1*n2)*n3/100,"\nWhere, your discount amount is: ", (n1*n2)*n3/100)
     elif b=="":
         break
